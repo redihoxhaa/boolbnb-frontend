9a40e7ada2826d533bef748b9b8da52e080776e1
redihoxhaa <redi.hoxha1996@gmail.com>
2024-03-14 15:24:44 +0100

    feat: mappa con radius

--- src/components/partials/MapReasearch.vue
+++ src/components/partials/MapReasearch.vue
@@ -14,7 +14,8 @@ export default {
     props: ['apartments', 'center'],
     data() {
         return {
-            store
+            store,
+            searchCircleLayer: null
         }
     },
     mounted() {
@@ -33,6 +34,16 @@ export default {
             map.addControl(new tt.FullscreenControl());
             map.addControl(new tt.NavigationControl());
 
+
+
+            // Attendi che lo stile della mappa sia completamente caricato
+            await new Promise((resolve) => {
+                map.on('load', resolve);
+            });
+
+            // Aggiungi cerchio di ricerca
+            this.addSearchCircle(map);
+
             for (const apartment of this.apartments) {
                 try {
                     let marker = new tt.Marker({
@@ -44,6 +55,12 @@ export default {
                     console.error('Error fetching coordinates:', error);
                 }
             }
+
+            // Aggiungi listener per l'evento di zoom
+            map.on('zoom', () => {
+                this.updateSearchCircleRadius(map);
+            });
+
         },
 
         createCustomMarkerElement() {
@@ -53,9 +70,57 @@ export default {
             markerElement.style.width = '52px'; // Imposta la larghezza desiderata
             markerElement.style.height = '52px'; // Imposta l'altezza desiderata
             return markerElement;
-        }
-    },
+        },
+        addSearchCircle(map) {
+            if (this.center && typeof this.center.radius === 'number') {
+                const centerCoordinate = new tt.LngLat(this.center.longitude, this.center.latitude);
+                const mapBounds = map.getBounds();
+                const mapWidth = mapBounds._ne.lng - mapBounds._sw.lng;
+                const mapHeight = mapBounds._ne.lat - mapBounds._sw.lat;
 
+                // Calcola la distanza in pixel in base alla larghezza della mappa
+                const pixelsPerMeter = 256 / mapWidth;
+                const radiusInMeters = this.center.radius * 1000; // Converti il raggio in metri
+                const radiusInPixels = radiusInMeters * pixelsPerMeter;
+
+                this.searchCircleLayer = map.addLayer({
+                    id: 'searchCircle',
+                    type: 'circle',
+                    source: {
+                        type: 'geojson',
+                        data: {
+                            type: 'Feature',
+                            geometry: {
+                                type: 'Point',
+                                coordinates: [this.center.longitude, this.center.latitude],
+                            },
+                            properties: {
+                                radius: radiusInPixels,
+                            },
+                        },
+                    },
+                    paint: {
+                        'circle-radius': radiusInPixels,
+                        'circle-color': 'rgba(0, 128, 0, 0.2)',
+                        'circle-stroke-color': 'rgba(0, 128, 0, 0.4)',
+                        'circle-stroke-width': 2,
+                    },
+                });
+            }
+        },
+
+        updateSearchCircleRadius(map) {
+            const zoom = map.getZoom();
+            const mapBounds = map.getBounds();
+            const latitude = map.getCenter().lat; // Ottieni la latitudine del centro della mappa
+
+            // Calcola il raggio del cerchio in pixel
+            const radiusInMeters = this.center.radius * 2300; // Converti il raggio in metri
+            const radiusInPixels = (radiusInMeters / (156543.03392 * Math.cos(latitude * Math.PI / 180))) * Math.pow(2, zoom);
+
+            map.setPaintProperty('searchCircle', 'circle-radius', radiusInPixels);
+        },
+    },
 };
 </script>
 
