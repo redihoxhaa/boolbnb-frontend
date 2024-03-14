0794eb4cd5512d73b940acbf887dfa8bd66b2ff2
WebGCDev <gaetano.chi@gmail.com>
2024-03-14 23:33:39 +0100

    feat: ApartmentCard

--- src/components/partials/ApartmentCard.vue
+++ src/components/partials/ApartmentCard.vue
@@ -1,195 +1,228 @@
 <script>
-import { store } from "../../store";
+import { store } from '../../store';
 import { DateTime } from 'luxon';
 
 export default {
-    name: 'ApartmentCard',
-    props: ['apartment', 'colSize'],
-    data() {
-        return {
-            props: [],
-            store
-        }
-    },
-    methods: {
-        showSponsoredTag(endDateStr) {
-            if (!endDateStr) return false; // Gestisci il caso in cui endDateStr è vuota
+  name: 'ApartmentCard',
+  props: ['apartment', 'colSize'],
+  data() {
+    return {
+      props: [],
+      store,
+    };
+  },
+  methods: {
+    showSponsoredTag(endDateStr) {
+      if (!endDateStr) return false; // Gestisci il caso in cui endDateStr è vuota
 
-            const currentDate = DateTime.now();
-            const endDate = DateTime.fromFormat(endDateStr, 'yyyy-MM-dd HH:mm:ss');
+      const currentDate = DateTime.now();
+      const endDate = DateTime.fromFormat(endDateStr, 'yyyy-MM-dd HH:mm:ss');
 
-            return endDate > currentDate;
-        },
-        // Funzione per visitare l'appartamento
-        visitApartment(apartmentID) {
-            this.$router.push({ name: "single-apartment", params: { id: apartmentID } });
-        },
+      return endDate > currentDate;
     },
-    mounted() {
-        this.showSponsoredTag();
-    }
-}
+
+    // Funzione per visitare l'appartamento
+    visitApartment(apartmentID) {
+      this.$router.push({
+        name: 'single-apartment',
+        params: { id: apartmentID },
+      });
+    },
+  },
+  mounted() {
+    this.showSponsoredTag();
+  },
+};
 </script>
 
 <template>
-    <div class="apartment-card" :class="colSize">
-        <div class="apartment-image-container m-0">
-            <div class="apartment-icon">
-                <div class="icon-sponsor">
-                    <div v-if="apartment.sponsorships && apartment.sponsorships.length" class="p-1">
-                        <div v-if="showSponsoredTag(apartment.sponsorships[apartment.sponsorships.length - 1].pivot.end_date)"
-                            class="px-5">
-                            Sponsored
-                        </div>
-                    </div>
-                </div>
-                <div class="icon-redirect">
-                    <div class=" arrow-icon" role="button" @click="visitApartment(apartment.id)">
-                        <img src="../../assets/img/arrow_card_icon.svg" alt="">
-                    </div>
-                </div>
+  <div class="apartment-card" :class="colSize">
+    <div class="apartment-image-container m-0">
+      <div class="apartment-icon">
+        <div class="icon-sponsor">
+          <div
+            v-if="apartment.sponsorships && apartment.sponsorships.length"
+            class="p-1"
+          >
+            <div
+              v-if="
+                showSponsoredTag(
+                  apartment.sponsorships[apartment.sponsorships.length - 1]
+                    .pivot.end_date
+                )
+              "
+              class="px-5"
+            >
+              Sponsored
             </div>
-            <div class="apartment-image">
-                <img :src="this.store.imagesAPI + apartment.images.split(',')[0]" alt="Apartment Image"
-                    v-if="apartment.images" />
-                <img src="https://victorsandbox.myshopify.com/cdn/shop/products/property-placeholder_grande.jpg?v=1482778691"
-                    class="card-image" alt="Apartment Image" v-else>
-            </div>
+          </div>
         </div>
-        <div class="apartment-info">
-            <div>
-                <h3>{{ apartment.title }}</h3>
-                <span class="truncate"> {{ apartment.address }}</span>
-            </div>
-            <ul class="d-flex mt-3 gap-4 list-unstyled align-items-center">
-                <li class="d-flex gap-2 align-items-center "><img class="rooms-icon"
-                        src="../../assets/img/rooms_icon.svg" alt="rooms"> {{
-        apartment.rooms }}</li>
-                <li class="d-flex gap-2 align-items-center"><img src="../../assets/img/beds_icon.svg" alt="beds"> {{
-        apartment.beds }}</li>
-                <li class="d-flex gap-2 align-items-center"><img class="bathroom-icon"
-                        src="../../assets/img/bathrooms_icon.svg" alt="bathrooms">
-                    {{
-                    apartment.bathrooms }}</li>
-            </ul>
+        <div class="icon-redirect">
+          <div
+            class="arrow-icon"
+            role="button"
+            @click="visitApartment(apartment.id)"
+          >
+            <img src="../../assets/img/arrow_card_icon.svg" alt="" />
+          </div>
         </div>
+      </div>
+      <div class="apartment-image">
+        <img
+          :src="this.store.imagesAPI + apartment.images.split(',')[0]"
+          alt="Apartment Image"
+          v-if="apartment.images"
+        />
+        <img
+          src="https://victorsandbox.myshopify.com/cdn/shop/products/property-placeholder_grande.jpg?v=1482778691"
+          class="card-image"
+          alt="Apartment Image"
+          v-else
+        />
+      </div>
     </div>
+    <div class="apartment-info">
+      <div>
+        <h3>{{ apartment.title }}</h3>
+        <span class="truncate"> {{ apartment.address }}</span>
+      </div>
+      <ul class="d-flex mt-3 gap-4 list-unstyled align-items-center">
+        <li class="d-flex gap-2 align-items-center">
+          <img
+            class="rooms-icon"
+            src="../../assets/img/rooms_icon.svg"
+            alt="rooms"
+          />
+          {{ apartment.rooms }}
+        </li>
+        <li class="d-flex gap-2 align-items-center">
+          <img src="../../assets/img/beds_icon.svg" alt="beds" />
+          {{ apartment.beds }}
+        </li>
+        <li class="d-flex gap-2 align-items-center">
+          <img
+            class="bathroom-icon"
+            src="../../assets/img/bathrooms_icon.svg"
+            alt="bathrooms"
+          />
+          {{ apartment.bathrooms }}
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 @use '../../assets/scss/partials/variables' as *;
 
-
 .apartment-card {
-    flex-shrink: 0;
-    position: relative;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
+  flex-shrink: 0;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 }
 
 .apartment-image-container {
-    width: 100%;
-    height: 320px;
-    border-radius: 18px;
-    overflow: hidden;
+  width: 100%;
+  height: 320px;
+  border-radius: 18px;
+  overflow: hidden;
 }
 
 .apartment-icon {
-    position: absolute;
-    width: 100%;
-    height: 320px;
+  position: absolute;
+  width: 100%;
+  height: 320px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  .icon-sponsor {
+    margin: 10px;
+
+    div {
+      div {
+        display: inline;
+        background-color: #cef27f;
+        padding: 10px 12px;
+        font-size: 12px;
+        border-radius: 10px;
+      }
+    }
+  }
+
+  .icon-redirect {
+    margin: 10px;
     display: flex;
-    flex-direction: column;
-    justify-content: space-between;
+    justify-content: end;
 
-    .icon-sponsor {
-        margin: 10px;
+    .arrow-icon {
+      width: 40px;
+      height: 40px;
+      text-align: center;
+      border-radius: 50%;
+      background-color: #fff;
+      display: flex;
+      align-items: center;
+      justify-content: center;
 
-        div {
-
-            div {
-                display: inline;
-                background-color: #CEF27F;
-                padding: 10px 12px;
-                font-size: 12px;
-                border-radius: 10px;
-            }
-        }
+      &:hover {
+        background-color: #f4f4f4;
+        transition: background-color 0.1s ease-in-out;
+      }
     }
-
-    .icon-redirect {
-        margin: 10px;
-        display: flex;
-        justify-content: end;
-
-
-
-
-        .arrow-icon {
-            width: 40px;
-            height: 40px;
-            text-align: center;
-            border-radius: 50%;
-            background-color: #fff;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-
-            &:hover {
-                background-color: #f4f4f4;
-                transition: background-color 0.1s ease-in-out;
-            }
-        }
-    }
+  }
 }
 
 .apartment-image {
+  width: 100%;
+  height: 100%;
+
+  img {
     width: 100%;
     height: 100%;
-
-    img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
+    object-fit: cover;
+  }
 }
 
 .apartment-info {
-    margin-top: 10px;
-    width: 100%;
-    flex-grow: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
+  margin-top: 10px;
+  width: 100%;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 
-    h3 {
-        font-size: 20px;
-        font-weight: 400;
-        line-height: 26px;
-    }
+  h3 {
+    font-size: 20px;
+    font-weight: 400;
+    line-height: 26px;
+  }
 
-    span {
-        font-size: 15px;
-        font-weight: 300;
-        color: $placeholder;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        display: inline-block;
-        /* Assicura che lo span rispetti la larghezza specificata */
-        max-width: 100%;
-        /* Imposta la larghezza massima dello span per evitare che superi il contenitore */
-        white-space: nowrap;
-    }
+  span {
+    font-size: 15px;
+    font-weight: 300;
+    color: $placeholder;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: inline-block;
 
+    /* Assicura che lo span rispetti la larghezza specificata */
+    max-width: 100%;
+
+    /* Imposta la larghezza massima dello span per evitare che superi il contenitore */
+
+    white-space: nowrap;
+  }
 }
 
 .rooms-icon {
-    padding-bottom: 2px;
+  padding-bottom: 2px;
 }
 
 .bathroom-icon {
-    padding-bottom: 2px;
+  padding-bottom: 2px;
 }
 </style>
