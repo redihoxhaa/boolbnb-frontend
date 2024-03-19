af7089dbb9c35481c2316821d601c4dc8eec4782
redihoxhaa <redi.hoxha1996@gmail.com>
2024-03-19 02:06:29 +0100

    feat: fixato about-us

--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -14,11 +14,11 @@ export default {
   <div class="custom-border">
 
     <div class="custom-container">
-      <h1 class="page-title pt-4">About Us</h1>
+      <h1 class="page-title pt-4">About <span class="highlighted">Us</span></h1>
       <div class="row g-3">
 
         <!-- Redi -->
-        <div class="col-6 col-md-4 col-xxl-5  about-us-card h-100">
+        <div class="col-6 col-md-4 col-xl-2 about-us-card h-100">
 
           <!-- Photo -->
           <div class="card-photo">
@@ -28,13 +28,13 @@ export default {
           <!-- Info -->
           <div class="card-info">
             <h3>Redi Hoxha</h3>
-            <span>Sesto San Giovanni, Lombardia</span>
+            <span class="pb-2">Milano, Lombardia</span>
 
             <!-- Social -->
-            <ul class="list-unstyled d-flex gap-2">
+            <ul class="list-unstyled d-flex gap-1">
 
               <!-- LinkedIn -->
-              <li class="me-3">
+              <li class="me-2">
                 <a href="https://www.linkedin.com/in/redihoxha/" target="_blank">
                   <img class="about-us-icon linkedin-icon" src="../assets/img/linkedin_logo.svg" alt="GitHub">
                 </a>
@@ -51,7 +51,7 @@ export default {
         </div>
 
         <!-- Vito -->
-        <div class="col-6 col-md-4 col-xxl-5  about-us-card h-100">
+        <div class="col-6 col-md-4 col-xl-2 about-us-card h-100">
 
           <!-- Photo -->
           <div class="card-photo">
@@ -61,13 +61,13 @@ export default {
           <!-- Info -->
           <div class="card-info">
             <h3>Vito D'Urso Pettersson</h3>
-            <span>Catania, Sicilia</span>
+            <span class="pb-2">Catania, Sicilia</span>
 
             <!-- Social -->
-            <ul class="list-unstyled d-flex gap-2">
+            <ul class="list-unstyled d-flex gap-1">
 
               <!-- LinkedIn -->
-              <li class="me-3">
+              <li class="me-2">
                 <a href="https://www.linkedin.com/in/vitodursopettersson/" target="_blank">
                   <img class="about-us-icon linkedin-icon" src="../assets/img/linkedin_logo.svg" alt="GitHub">
                 </a>
@@ -84,7 +84,7 @@ export default {
         </div>
 
         <!-- Mirko -->
-        <div class="col-6 col-md-4 col-xxl-5  about-us-card h-100">
+        <div class="col-6 col-md-4 col-xl-2 about-us-card h-100">
 
           <!-- Photo -->
           <div class="card-photo">
@@ -94,13 +94,13 @@ export default {
           <!-- Info -->
           <div class="card-info">
             <h3>Mirko Cherchi</h3>
-            <span>Roma, Lazio</span>
+            <span class="pb-2">Roma, Lazio</span>
 
             <!-- Social -->
-            <ul class="list-unstyled d-flex gap-2">
+            <ul class="list-unstyled d-flex gap-1">
 
               <!-- LinkedIn -->
-              <li class="me-3">
+              <li class="me-2">
                 <a href="https://www.linkedin.com/in/mirko-cherchi/" target="_blank">
                   <img class="about-us-icon linkedin-icon" src="../assets/img/linkedin_logo.svg" alt="GitHub">
                 </a>
@@ -117,7 +117,7 @@ export default {
         </div>
 
         <!-- Lorenzo -->
-        <div class="col-6 col-md-4 col-xxl-5  about-us-card h-100">
+        <div class="col-6 col-md-4 col-xl-2 about-us-card h-100">
 
           <!-- Photo -->
           <div class="card-photo">
@@ -127,13 +127,13 @@ export default {
           <!-- Info -->
           <div class="card-info">
             <h3>Lorenzo De Vito</h3>
-            <span>Roma, Lazio</span>
+            <span class="pb-2">Roma, Lazio</span>
 
             <!-- Social -->
-            <ul class="list-unstyled d-flex gap-2">
+            <ul class="list-unstyled d-flex gap-1">
 
               <!-- LinkedIn -->
-              <li class="me-3">
+              <li class="me-2">
                 <a href="https://www.linkedin.com/in/lorenzo-devito/" target="_blank">
                   <img class="about-us-icon linkedin-icon" src="../assets/img/linkedin_logo.svg" alt="GitHub">
                 </a>
@@ -150,7 +150,7 @@ export default {
         </div>
 
         <!-- Gaetano -->
-        <div class="col-6 col-md-4 col-xxl-5  about-us-card h-100">
+        <div class="col-6 col-md-4 col-xl-2 about-us-card h-100">
 
           <!-- Photo -->
           <div class="card-photo">
@@ -160,13 +160,46 @@ export default {
           <!-- Info -->
           <div class="card-info">
             <h3>Gaetano Chiapparino</h3>
-            <span></span>
+            <span class="pb-2">Bari, Puglia</span>
 
             <!-- Social -->
-            <ul class="list-unstyled d-flex gap-2">
+            <ul class="list-unstyled d-flex gap-1">
 
               <!-- LinkedIn -->
-              <li class="me-3">
+              <li class="me-2">
+                <a href="https://github.com/tuoprofilogithub" target="_blank">
+                  <img class="about-us-icon linkedin-icon" src="../assets/img/linkedin_logo.svg" alt="GitHub">
+                </a>
+              </li>
+
+              <!-- GitHub -->
+              <li>
+                <a href="https://github.com/WebGCDev" target="_blank">
+                  <img class="about-us-icon" src="../assets/img/github_logo.svg" alt="LinkedIn">
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <!-- Roberto -->
+        <div class="col-6 col-md-4 col-xl-2 about-us-card h-100">
+
+          <!-- Photo -->
+          <div class="card-photo">
+            <img class="img-fluid" src="../assets/img/roberto-malone.webp" alt="">
+          </div>
+
+          <!-- Info -->
+          <div class="card-info">
+            <h3>Roberto Malone</h3>
+            <span class="pb-2">Torino, Piemonte</span>
+
+            <!-- Social -->
+            <ul class="list-unstyled d-flex gap-1">
+
+              <!-- LinkedIn -->
+              <li class="me-2">
                 <a href="https://github.com/tuoprofilogithub" target="_blank">
                   <img class="about-us-icon linkedin-icon" src="../assets/img/linkedin_logo.svg" alt="GitHub">
                 </a>
@@ -191,21 +224,47 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+@use '../assets/scss/partials/variables' as *;
+
+.about-us-card {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.card-info {
+  position: relative;
+}
+
+
 .card-photo {
   // aspect-ratio: 1;
+  overflow: hidden;
+  border-radius: 16px;
 
   img {
     // height: 100%;
     object-fit: cover;
+    width: 100%;
+    filter: saturate(0);
   }
 }
 
 .about-us-icon {
   width: 20px;
+  filter: opacity(0.7);
 }
 
 .linkedin-icon {
   width: 16px;
+}
+
+.list-unstyled {
+  position: absolute;
+  filter: invert(1);
+  top: -43px;
+  right: 10px;
 }
 
 .custom-border {
@@ -218,7 +277,7 @@ export default {
 }
 
 .page-title {
-  font-size: 36px;
+  font-size: 50px;
   margin-bottom: 20px;
   color: #333;
   text-align: left;
@@ -227,6 +286,10 @@ export default {
 .custom-container {
   padding: 0 10vw;
   margin-bottom: 40px;
+}
+
+.highlighted {
+  color: $acid-yellow;
 }
 
 .card-info {
@@ -247,13 +310,6 @@ export default {
     font-size: 15px;
     font-weight: 300;
     display: inline-block;
-  }
-}
-
-// Medium devices (tablets, 768px and up)
-@media (min-width: 992px) {
-  .about-us-card {
-    width: calc(100% / 5);
   }
 }
 </style>
